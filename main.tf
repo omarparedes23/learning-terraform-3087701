f5dfa16613d19253b3bbfbfa32b768e325771b78
omarparedes23 <45073928+omarparedes23@users.noreply.github.com>
2024-04-30 18:50:02 +0200

    agregando2

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.id.security_group_id]
 
   tags = {
     Name = "Learning Terraform"
