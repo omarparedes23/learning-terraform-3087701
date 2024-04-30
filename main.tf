9e045373db5f5ffa5528c1e95c44c0c036ad7014
omarparedes23 <45073928+omarparedes23@users.noreply.github.com>
2024-04-30 18:31:57 +0200

    corregir error1

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "app_ami" {
 }
 
 data "aws_vpc" "default" {
-  default=true
+  default = true
 }
 
 resource "aws_instance" "blog" {
@@ -28,20 +28,7 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
-  name    ="blog_new"
 
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-
-}
 
 resource "aws_security_group" "blog" {
   name        = "blog"
